50bbe98187a23f8e1023322b98fe7503e9596ead
Davi Cesar <davicesarmg@hotmail.com>
2023-02-28 19:25:20 -0300

    adjustment

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,48 +1,48 @@
 package entities;
 
 public class Account {
-    private int number;
-    private String holder;
-    private double balance;
 
-    public Account(int number, String holder) {
-        this.number = number;
-        this.holder = holder;
-    }
+  private int number;
+  private String holder;
+  private double balance;
 
-    public Account(int number, String holder, double initialDeposit) {
-        this.number = number;
-        this.holder = holder;
-        deposit(initialDeposit);
-    }
+  public Account(int number, String holder) {
+    this.number = number;
+    this.holder = holder;
+  }
 
-    public int getNumber() {
-        return number;
-    }
+  public Account(int number, String holder, double initialDeposit) {
+    this.number = number;
+    this.holder = holder;
+    deposit(initialDeposit);
+  }
 
-    public String getHolder() {
-        return holder;
-    }
+  public int getNumber() {
+    return number;
+  }
 
-    public void setHolder(String holder) {
-        this.holder = holder;
-    }
+  public String getHolder() {
+    return holder;
+  }
 
-    public double getBalance() {
-        return balance;
-    }
+  public void setHolder(String holder) {
+    this.holder = holder;
+  }
 
-    public void deposit(double amount){
-        balance += amount;
-    }
-    public void withdraw(double amount){
-        balance -= amount + 5.0;
-    }
+  public double getBalance() {
+    return balance;
+  }
 
-    @Override
-    public String toString() {
-        return "Id: " + number +
-                "\nHolder: " + holder +
-                "\nBalance: $" + balance;
-    }
+  public void deposit(double amount) {
+    balance += amount;
+  }
+
+  public void withdraw(double amount) {
+    balance -= amount + 5.0;
+  }
+
+  @Override
+  public String toString() {
+    return "Id: " + number + "\nHolder: " + holder + "\nBalance: $" + balance;
+  }
 }
